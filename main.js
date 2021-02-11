b87d69a67f596f4afea7cc09adc066e1679e87e3
Rithvik39 <72606950+Rithvik39@users.noreply.github.com>
2021-02-11 17:31:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+var canvas= document.getElementById("canvas1");
+var ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle= "grey";
+ctx.lineWidth= 4;
+ctx.rect(150,143,430,200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "blue";
+ctx.lineWidth= 5;
+ctx.arc(260,210,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "black";
+ctx.lineWidth= 5;
+ctx.arc(360,210,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth= 5;
+ctx.arc(460,210,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "yellow";
+ctx.lineWidth= 5;
+ctx.arc(310,255,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "green";
+ctx.lineWidth= 5;
+ctx.arc(410,255,40,0,2 * Math.PI);
+ctx.stroke();
